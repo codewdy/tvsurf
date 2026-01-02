f88a71ca0042b448e765622544b9d82585e1946f
codewdy <codewdy@gmail.com>
2026-01-02 21:07:26 +0800

    add searchers

---
+++ service/searcher/searcher.py
@@ -0,0 +1,69 @@
+from .subject import create_subject_searcher
+from .channel import create_channel_searcher
+from service.schema.tvdb import Source, SourceUrl
+import asyncio
+
+
+class Searcher:
+    def __init__(self, config: dict):
+        self.key = config["key"]
+        self.name = config["name"]
+        self.subject_searcher = create_subject_searcher(
+            config["subject_searcher"])
+        self.channel_searcher = create_channel_searcher(
+            config["channel_searcher"])
+
+    async def search(self, keyword: str) -> list[Source]:
+        try:
+            results = []
+            subjects = await self.subject_searcher.search(keyword)
+            all_channels = await asyncio.gather(*[self.channel_searcher.search(subject.url) for subject in subjects])
+            for subject, channels in zip(subjects, all_channels):
+                channels = await self.channel_searcher.search(subject.url)
+                for channel in channels:
+                    results.append(Source(
+                        source=SourceUrl(
+                            source_key=self.key,
+                            source_name=self.name,
+                            channel_name=channel.name,
+                            url=subject.url,
+                        ),
+                        name=subject.name,
+                        cover_url=subject.cover_url or channel.cover_url,
+                        episodes=[Source.Episode(
+                            source=SourceUrl(
+                                source_key=self.key,
+                                source_name=self.name,
+                                channel_name=channel.name,
+                                url=e.url,
+                            ),
+                            name=e.name,
+                        ) for e in channel.episodes],
+                    ))
+            return results
+        except:
+            raise RuntimeError("search error: ", self.name, keyword)
+
+
+if __name__ == "__main__":
+    import json
+    import asyncio
+    import sys
+    from service.lib.context import Context, searcher_config_path
+
+    src = sys.argv[-2]
+    keyword = sys.argv[-1]
+    with open(searcher_config_path(), "r") as f:
+        searcher_config = json.load(f)
+    searcher_config = [
+        i for i in searcher_config["searchers"] if i["key"] == src][0]
+    searcher = Searcher(searcher_config)
+
+    async def run():
+        async with Context():
+            rst = await searcher.search(keyword)
+            print(rst)
+            with open("result.json", "w") as f:
+                f.write(json.dumps([i.model_dump(mode="json")
+                        for i in rst], ensure_ascii=False, indent=2))
+    asyncio.run(run())
---
+++ service/searcher/searchers.py
@@ -0,0 +1,45 @@
+from functools import cache
+import asyncio
+from service.lib.context import Context
+from service.lib.context import searcher_config_path
+from .searcher import Searcher
+from pathlib import Path
+import json
+from service.schema.tvdb import Source
+
+
+@cache
+def searcher_list():
+    with open(searcher_config_path(), "r") as f:
+        searcher_config = json.load(f)
+    return [Searcher(config) for config in searcher_config["searchers"] if config["enable"]]
+
+
+class Searchers:
+    def __init__(self):
+        self.searchers = searcher_list()
+        self.searcher_dict = {
+            searcher.key: searcher for searcher in self.searchers}
+
+    async def search(self, keyword: str) -> list[Source]:
+        results = await asyncio.gather(*[searcher.search(keyword) for searcher in self.searchers])
+        return sum(results, [])
+
+
+if __name__ == "__main__":
+    import json
+    import asyncio
+    import sys
+    from service.lib.context import Context, searcher_config_path
+
+    keyword = sys.argv[-1]
+
+    async def run():
+        async with Context():
+            searchers = Searchers()
+            rst = await searchers.search(keyword)
+            print(rst)
+            with open("result.json", "w") as f:
+                f.write(json.dumps([i.model_dump(mode="json")
+                        for i in rst], ensure_ascii=False, indent=2))
+    asyncio.run(run())
