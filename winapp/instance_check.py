2c86eee9833020649b74fe2f7234f59318c17ad4
codewdy <codewdy@gmail.com>
2026-01-27 09:11:48 +0800

    use library path to instance-check

--- winapp/instance_check.py
+++ winapp/instance_check.py
@@ -1,10 +1,23 @@
 import filelock
 import os
 import sys
+import platform
 
 
 def default_lock_path():
-    if hasattr(sys, "_MEIPASS"):
+    if (
+        platform.system() == "Darwin"
+        and hasattr(sys, "_MEIPASS")
+        and ".app" in sys.argv[0]
+    ):
+        return os.path.join(
+            os.path.expanduser("~"),
+            "Library",
+            "Application Support",
+            "com.codewdy.tvsurf",
+            "instance.lock",
+        )
+    elif hasattr(sys, "_MEIPASS"):
         return os.path.join(os.path.dirname(sys.argv[0]), "instance.lock")
     else:
         return "instance.lock"
