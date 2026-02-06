64aaa94cdefe8fe1157391cd273bc138dbbfe9b0
codewdy <codewdy@gmail.com>
2026-02-06 19:59:22 +0800

    update max concurrent fragments and downloads in DownloadConfig

--- service/schema/config.py
+++ service/schema/config.py
@@ -5,8 +5,8 @@ from enum import Enum
 class DownloadConfig(BaseModel):
     connect_timeout: TimeDelta = "1m"  # type: ignore
     chunk_size: ByteSize = "1MB"  # type: ignore
-    max_concurrent_fragments: int = 5
-    max_concurrent_downloads: int = 3
+    max_concurrent_fragments: int = 1
+    max_concurrent_downloads: int = 5
     max_retries: int = 3
     download_timeout: TimeDelta = "1h"  # type: ignore
     retry_interval: TimeDelta = "1m"  # type: ignore
