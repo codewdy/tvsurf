6a8d0466c9cca068d6c50c4fc973dd06ef66b122
codewdy <codewdy@gmail.com>
2026-05-19 19:52:00 +0800

    fix raw resource searcher

--- service/searcher/resource/raw.py
+++ service/searcher/resource/raw.py
@@ -2,8 +2,5 @@ from .base import BaseResourceSearcher
 
 
 class RawResourceSearcher(BaseResourceSearcher):
-    def __init__(self):
-        pass
-
     async def search_impl(self, url):
         return url
